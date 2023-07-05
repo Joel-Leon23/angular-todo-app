e19bc699b956a5a0012751a5769065b5e3e43567
Joel Leon <joel.leonprz@uanl.edu.mx>
2023-07-05 13:50:17 -0600

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Product } from './models/product.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-training';
+  http = inject(HttpClient)
+  products: Product[] = []
+
+  setTitle(title: string = 'new title') {
+    this.title = title
+  }
+
+  ngOnInit() {
+    this.http.get<Product[]>('https://api.escuelajs.co/api/v1/products').subscribe((data) => {
+      this.products = data
+    })
+  }
 }
