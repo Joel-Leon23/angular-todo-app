efa5353069a0e476dd0d0386a94d9f121928086e
Joel Leon <joel.leonprz@uanl.edu.mx>
2023-07-06 13:06:53 -0600

    Add task

---
+++ src/app/components/add-task/add-task.component.ts
@@ -0,0 +1,34 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Task } from 'src/app/models/Task';
+
+@Component({
+  selector: 'app-add-task',
+  templateUrl: './add-task.component.html',
+  styleUrls: ['./add-task.component.css']
+})
+export class AddTaskComponent {
+  @Output() onAddTask: EventEmitter<Task> = new EventEmitter()
+
+  text: string = "";
+  day: string = "";
+  reminder: boolean = false;
+
+
+  onSubmit() {
+    if (!this.text) {
+      alert('Please add task!')
+      return
+    }
+    const newTask = {
+      text: this.text,
+      day: this.day,
+      reminder: this.reminder
+    }
+
+    this.onAddTask.emit(newTask)
+
+    this.text = ''
+    this.day = ''
+    this.reminder = false
+  }
+}
